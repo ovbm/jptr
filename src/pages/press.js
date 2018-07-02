9a85e79109dffe3bbbe909981d95cdfcfab2ef71
Olivier Baumann <olivier@iovi.io>
2018-07-02 09:11:40 +0200

    header fix press

--- src/pages/press.js
+++ src/pages/press.js
@@ -107,7 +107,6 @@ class Press extends React.Component {
               height="450"
               scrolling="no"
               frameBorder="no"
-              allow="autoplay"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/552555507%3Fsecret_token%3Ds-uRwXt&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" />
             <h2 id="about">ABOUT</h2>
             <p style={{ textAlign: "left" }}> 
