21380823914c5ab71ce70bc42a87d6ea235e2446
Olivier Baumann <olivier@iovi.io>
2020-06-19 20:40:55 +0200

    Updates

---
+++ component---src-pages-404-js-3494a797e7a23724bb45.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J");t.default=function(e){return l.a.createElement(o.a,{location:e.location},l.a.createElement("div",{className:"container"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3494a797e7a23724bb45.js.map
